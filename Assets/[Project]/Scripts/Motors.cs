f4681dd35fac9186d2806f906957cb702a6fff8a
Arthur Caumartin <artcaumart@gmail.com>
2023-12-07 17:16:37 +0100

    Change motore + ratio fct

--- Assets/[Project]/Scripts/Motors.cs
+++ Assets/[Project]/Scripts/Motors.cs
@@ -4,19 +4,35 @@ using UnityEngine;
 
 public class Motors : MonoBehaviour
 {
-    [SerializeField] private float _heatValue;
+    [SerializeField] private float _heatSpeed;
+    [SerializeField] private float _heatDecay;
+    [SerializeField] private float _timeBeforExplode = 10f;
+    private float _heatValue;
 
     public void UpdateHeat(float speedFactor)
     {
-        print(speedFactor);
+        print("Speed factor = " + speedFactor);
         if(speedFactor > .9f)
         {
-            _heatValue += Time.deltaTime;
+            _heatValue += Time.deltaTime * _heatSpeed;
+        }
+        else
+        {
+            if(_heatValue > 0)
+                _heatValue -= Time.deltaTime * _heatDecay;
         }
 
-        if(_heatValue > 1)
+        if(_heatValue > _timeBeforExplode)
         {
-            Destroy(gameObject);
+            if(GameManager.instance)
+                GameManager.instance.ShipExplode();
         }
+
+        print(GetHeatRatio());
+    }
+
+    public float GetHeatRatio()
+    {
+        return Mathf.InverseLerp(0, _timeBeforExplode, _heatValue);
     }
 }
